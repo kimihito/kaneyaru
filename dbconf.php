6f6f30efcf2a6c9d90cc18f2e03646c38c2421c1
kimihito <tatsurotamashiro@gmail.com>
2012-04-05 03:24:09 +0900

    dattabase conf

--- dbconf.php
+++ dbconf.php
@@ -1,7 +1,7 @@
 <?php
 define("HOST", 'localhost');
-define("USERNAME", 'yume');
-define("PASSWORD", 'yumekaki');
+define("USERNAME", 'hoge');
+define("PASSWORD", 'hogehoge');
 define("TABLE", 'kaneyaru');
 
 ?>
